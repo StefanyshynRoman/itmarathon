e1968964aca3b86de517c8acfcd40a0537710975
roman <romanpz051@gmail.com>
2025-11-11 15:57:55 +0100

    (Homework)

--- testautomation/SecretNick.TestAutomation/Tests/Ui/Pages/RoomManagementPage.cs
+++ testautomation/SecretNick.TestAutomation/Tests/Ui/Pages/RoomManagementPage.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// testautomation/SecretNick.TestAutomation/Tests/Ui/Pages/RoomManagementPage.cs
+using Microsoft.Playwright;
 
-namespace Tests.Ui.Pages
+namespace SecretNick.TestAutomation.Tests.Ui.Pages;
+
+public class RoomManagementPage
 {
-    internal class RoomManagementPage
+    private readonly IPage _page;
+
+    // Конструктор
+    public RoomManagementPage(IPage page)
     {
+        _page = page;
+    }
+
+    // Локатори (приклади, вам потрібно буде знайти справжні)
+    private ILocator UserRow(string userName) =>
+        _page.Locator($"//tr[contains(., '{userName}')]");
+
+    private ILocator DeleteButtonForUser(string userName) =>
+        UserRow(userName).Locator("button.delete-user-btn"); // Приклад CSS селектора
+
+    // Методи (Дії)
+    public async Task DeleteUser(string userName)
+    {
+        await DeleteButtonForUser(userName).ClickAsync();
+        // Тут можна додати логіку підтвердження (натискання "Так" у діалоговому вікні)
+        // await _page.Locator("#confirm-delete-btn").ClickAsync();
+    }
+
+    public async Task<bool> IsUserVisible(string userName)
+    {
+        return await UserRow(userName).IsVisibleAsync();
+    }
+
+    public async Task<bool> IsDeleteButtonDisabled(string userName)
+    {
+        return await DeleteButtonForUser(userName).IsDisabledAsync();
     }
 }
--- testautomation/SecretNick.TestAutomation/Tests/Ui/Steps/UserDeletionSteps.cs
+++ testautomation/SecretNick.TestAutomation/Tests/Ui/Steps/UserDeletionSteps.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// testautomation/SecretNick.TestAutomation/Tests/Ui/Steps/UserDeletionSteps.cs
+using FluentAssertions;
+using Reqnroll;
+using SecretNick.TestAutomation.Tests.Ui.Pages;
+using Shouldly;
+using Tests.Core.Drivers;
 
-namespace Tests.Ui.Steps
+namespace SecretNick.TestAutomation.Tests.Ui.Steps;
+
+[Binding]
+public class UserDeletionSteps
 {
-    internal class UserDeletionSteps
+    private readonly RoomManagementPage _roomPage;
+
+    // DI автоматично "вставить" сюди потрібні об'єкти
+    public UserDeletionSteps(BrowserDriver browserDriver)
     {
+        _roomPage = new RoomManagementPage(browserDriver.Page);
+    }
+
+    // --- КРОКИ ДЛЯ СЦЕНАРІЇВ ---
+
+    [Given(@"I am logged in as an ""(.*)""")]
+    public void GivenIAmLoggedInAsAn(string role)
+    {
+        // Тут має бути логіка логіну (ймовірно, вона вже є в інших Step-файлах)
+        // Наприклад: _loginPage.Login(user, pass);
+        // Поки що можна пропустити, якщо припустити, що ми вже залогінені
+    }
+
+    [Given(@"I navigate to the ""(.*)"" management page")]
+    public async Task GivenINavigateToTheRoomManagementPage(string roomName)
+    {
+        // Логіка переходу на сторінку кімнати
+        // await _page.GotoAsync("http://localhost:8081/room/123");
+    }
+
+    [Given(@"user ""(.*)"" is visible in the list")]
+    public async Task GivenUserIsVisibleInTheList(string userName)
+    {
+        (await _roomPage.IsUserVisible(userName)).Should().BeTrue();
+    }
+
+    [When(@"I click the delete button for ""(.*)""")]
+    public async Task WhenIClickTheDeleteButtonFor(string userName)
+    {
+        await _roomPage.DeleteUser(userName);
+    }
+
+    [Then(@"user ""(.*)"" should no longer be visible in the list")]
+    public async Task ThenUserShouldNoLongerBeVisibleInTheList(string userName)
+    {
+        (await _roomPage.IsUserVisible(userName)).Should().BeFalse();
+    }
+
+    [Then(@"the delete button for ""(.*)"" should be disabled")]
+    public async Task ThenTheDeleteButtonForShouldBeDisabled(string userName)
+    {
+        (await _roomPage.IsDeleteButtonDisabled(userName)).Should().BeTrue();
     }
 }
